beb78df10e2913256c39e1b64b5072fd63b3f1ec
lukasz-drzewiecki <lukasz.drzewiecki@mosaic-lab.com>
2015-09-02 15:33:31 +0200

    Re "Add support for new KYC call without UserId" - unit test added

---
+++ MangoPay.SDK.Tests/ApiKycTest.cs
@@ -0,0 +1,29 @@
+﻿using MangoPay.SDK.Entities.GET;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MangoPay.SDK.Tests
+{
+    [TestClass]
+    public class ApiKycTest : BaseTest
+    {
+        [TestMethod]
+        public void Test_GetKycDocument()
+        {
+            try
+            {
+				KycDocumentDTO kycDocument = this.GetJohnsKycDocument();
+				KycDocumentDTO result = this.Api.Kyc.Get(kycDocument.Id);
+
+				Assert.IsNotNull(result);
+				Assert.IsTrue(result.Id.Equals(kycDocument.Id));
+				Assert.IsTrue(result.Status == kycDocument.Status);
+				Assert.IsTrue(result.Type == kycDocument.Type);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
