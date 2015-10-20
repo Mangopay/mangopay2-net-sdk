d8d46dd53f0c0c62d0065d3ac556c195732b9ab1
lukasz-drzewiecki <lukasz.drzewiecki@mosaic-lab.com>
2015-10-20 15:31:00 +0200

    Updated DisputeContestPutDTO class

--- MangoPay.SDK/Entities/PUT/DisputeContestPutDTO.cs
+++ MangoPay.SDK/Entities/PUT/DisputeContestPutDTO.cs
@@ -1,4 +1,7 @@
-﻿
+﻿using MangoPay.SDK.Core.Enumerations;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
 namespace MangoPay.SDK.Entities.PUT
 {
     /// <summary>Dispute contest PUT entity.</summary>
@@ -6,5 +9,9 @@ namespace MangoPay.SDK.Entities.PUT
     {
 		/// <summary>The amount you wish to contest (must be the same currency as the DisputedFunds, and the amount can be up to and including the DisputedFunds).</summary>
 		public Money ContestedFunds { get; set; }
+
+		/// <summary>Status of dispute.</summary>
+		[JsonConverter(typeof(StringEnumConverter))]
+		public DisputeStatus? Status { get; set; }
     }
 }
