684d2661f36472b82ba5a3097555a05db0e0b4a0
lukasz-drzewiecki <lukasz.drzewiecki@mosaic-lab.com>
2016-09-05 14:59:51 +0200

    Update of: Prepared SDK to be able to talk to different APIs per instance configuration.

---
+++ MangoPay.SDK/Entities/GET/BankAccountUsObsoleteDTO.cs
@@ -0,0 +1,21 @@
+﻿using MangoPay.SDK.Core.Enumerations;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+
+namespace MangoPay.SDK.Entities.GET
+{
+    /// <summary>Class represents US type of bank account.</summary>
+    public class BankAccountUsObsoleteDTO : BankAccountObsoleteDTO
+    {
+        /// <summary>Account number.</summary>
+        public String AccountNumber { get; set; }
+
+        /// <summary>ABA.</summary>
+        public String ABA { get; set; }
+
+		/// <summary>Deposit account type.</summary>
+		[JsonConverter(typeof(StringEnumConverter))]
+		public DepositAccountType DepositAccountType { get; set; }
+    }
+}
