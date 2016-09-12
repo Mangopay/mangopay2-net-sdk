2c8ad89c706fe99345dd8900279b5d8932d54993
Gareth Thackeray <gareththackeray@gmail.com>
2016-09-12 16:33:18 +0100

    Store information about the last request/response, including pulling rate-limiting info from the headers.

---
+++ MangoPay.SDK/Core/LastRequestInfo.cs
@@ -0,0 +1,13 @@
+﻿using RestSharp;
+
+namespace MangoPay.SDK.Core
+{
+    public class LastRequestInfo
+    {
+        public IRestRequest Request { get; set; }    
+        public IRestResponse Response { get; set; }
+        public string RateLimitingCallsAllowed { get; set; }
+        public string RateLimitingCallsRemaining { get; set; }
+        public string RateLimitingTimeTillReset { get; set; }
+    }
+}
