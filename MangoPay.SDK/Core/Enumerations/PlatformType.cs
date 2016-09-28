1d3fbbf02c8d14402f65395b42c2b5ed23711018
Lukasz Puslecki <lukasz.puslecki@mosaic-lab.com>
2016-09-28 12:31:00 +0200

    Add NotSpecified enum value for PlatformType

--- MangoPay.SDK/Core/Enumerations/PlatformType.cs
+++ MangoPay.SDK/Core/Enumerations/PlatformType.cs
@@ -1,7 +1,9 @@
 ﻿namespace MangoPay.SDK.Core.Enumerations
 {
+	/// <summary>Client PlatformT type enumeration.</summary>
 	public enum PlatformType
 	{
+		NotSpecified,
 		MARKETPLACE,
 		P2P_PAYMENT,
 		CROWDFUNDING_DONATION,
