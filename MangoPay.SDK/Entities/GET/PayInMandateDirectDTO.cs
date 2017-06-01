c0600d2f565a84e5a23c1dec13d86ff73666bfe7
Lukasz Puslecki <lukasz.puslecki@mosaic-lab.com>
2017-06-01 12:19:45 +0200

    Add a new property called ChargeDate to the Payin Direct Debit Direct object.

--- MangoPay.SDK/Entities/GET/PayInMandateDirectDTO.cs
+++ MangoPay.SDK/Entities/GET/PayInMandateDirectDTO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MangoPay.SDK.Core;
+using Newtonsoft.Json;
+using System;
 
 namespace MangoPay.SDK.Entities.GET
 {
@@ -10,5 +12,9 @@ namespace MangoPay.SDK.Entities.GET
 
 	/// <summary>An optional value to be specified on the user's bank statement.</summary>
         public String StatementDescriptor { get; set; }
-    }
+		
+		/// <summary>Charge date.</summary>
+		[JsonConverter(typeof(UnixDateTimeConverter))]
+		public DateTime ChargeDate { get; set; }
+	}
 }
