cc4d12c99fb0061aa684483e2fe765646696f428
3siekm <3siekm@gmail.com>
2017-07-10 11:19:32 +0200

    Fill capacity property in UserNaturalPostDTO for tests

--- MangoPay.SDK.Tests/BaseTest.cs
+++ MangoPay.SDK.Tests/BaseTest.cs
@@ -153,6 +153,7 @@ namespace MangoPay.SDK.Tests
                 user.Occupation = "programmer";
                 user.IncomeRange = 3;
 				user.Address = new Address { AddressLine1 = "Address line 1", AddressLine2 = "Address line 2", City = "City", Country = CountryIso.PL, PostalCode = "11222", Region = "Region" };
+				user.Capacity = CapacityType.DECLARATIVE;
 
                 BaseTest._john = this.Api.Users.Create(user);
 
@@ -167,8 +168,9 @@ namespace MangoPay.SDK.Tests
             user.Occupation = "programmer";
             user.IncomeRange = 3;
 			user.Address = new Address { AddressLine1 = "Address line 1", AddressLine2 = "Address line 2", City = "City", Country = CountryIso.PL, PostalCode = "11222", Region = "Region" };
+			user.Capacity = CapacityType.DECLARATIVE;
 
-            return this.Api.Users.Create(user);
+			return this.Api.Users.Create(user);
         }
 
         protected UserLegalDTO GetMatrix()
