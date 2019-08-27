579105201afd8bca36b98d1103fa3addad8e2d25
Jean-Baptiste Vovau <jb.vovau@gmail.com>
2019-08-27 16:36:15 +0200

    Complete the "Debited Bank Account" object with IBAN / BIC informations.

--- MangoPay.SDK/Entities/GET/DebitedBankAccountDTO.cs
+++ MangoPay.SDK/Entities/GET/DebitedBankAccountDTO.cs
@@ -4,7 +4,22 @@ namespace MangoPay.SDK.Entities.GET
 {
 	public class DebitedBankAccountDTO
 	{
-		/// <summary>Owner name.</summary>
-		public String OwnerName { get; set; }
+        /// <summary>The name of the owner of the bank account</summary>
+		public string OwnerName { get; set; }
+
+        /// <summary>
+        /// The type of bank account
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// The IBAN of the bank account
+        /// </summary>
+        public string IBAN { get; set; }
+
+        /// <summary>
+        /// The BIC of the bank account
+        /// </summary>
+        public string BIC { get; set; }
 	}
 }
