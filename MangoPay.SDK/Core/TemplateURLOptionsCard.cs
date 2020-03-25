cf85291dac30214402f7a573a2b95efd415ef9d7
Mickaël POIS <mickael@mangopay.com>
2020-03-25 16:01:25 +0100

    Update TemplateURLOptionsCard.cs

--- MangoPay.SDK/Core/TemplateURLOptionsCard.cs
+++ MangoPay.SDK/Core/TemplateURLOptionsCard.cs
@@ -1,1 +1,15 @@
+using System;
 
+namespace MangoPay.SDK.Core
+{
+    /// <summary>Template URL options Card class.</summary>
+    public class TemplateURLOptionsCard
+    {
+        /// <summary>PAYLINE: will be deprecated on April 30th in production and ignored. Please use PAYLINEV2 parameter</summary>
+        [ObsoleteAttribute("PAYLINE attribute is now obsolete. Please use PAYLINEV2 instead", true)] 
+        public String PAYLINE;
+        
+        /// <summary>PAYLINEV2: should be set to apply your own template on our new payment widget for Payin Card Web</summary>
+        public String PAYLINEV2;
+    }
+}
