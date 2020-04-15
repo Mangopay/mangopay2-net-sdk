6450c3ee727a12cd9aa43913669fa41ee37aa056
Mircea Solovăstru <mircea.solovastru@codegile.com>
2020-04-15 08:44:32 +0300

    changed script for nuget

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-PACKAGE_FILE=`find . -type f -name mangopay2-sdk.*.nupkg | head -n 1`
+PACKAGE_FILE=`find . -type f -name MangoPay.SDK.*.nupkg | head -n 1`
 nuget push $PACKAGE_FILE -ApiKey $NUGET_API_KEY -Source $NUGET_SOURCE
