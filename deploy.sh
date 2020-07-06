98aa8f2b9c06acd221f69daaee1bfb3a3f3f4487
SoloJr <mircea.solo95@yahoo.com>
2020-07-06 11:10:04 +0300

    changed something in script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nuget setapikey $NUGET_API_KEY -Source $NUGET_URL
+nuget setapikey $NUGET_API_KEY -Source $NUGET_SOURCE
 dotnet nuget push ./MangoPay.SDK/bin/Release/mangopay2-sdk.*.nupkg $NUGET_API_KEY -s $NUGET_SOURCE
