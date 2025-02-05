9cbc690a0bdbceb75b4e2a46f3beb4fc10f26c95
Iulian Masar <iulian.masar@codegile.com>
2025-02-05 15:22:40 +0200

    more fixes

--- MangoPay.SDK.Tests/ApiEventsTest.cs
+++ MangoPay.SDK.Tests/ApiEventsTest.cs
@@ -1,10 +1,10 @@
-﻿using MangoPay.SDK.Core;
+﻿using System;
+using System.Threading.Tasks;
+using MangoPay.SDK.Core;
 using MangoPay.SDK.Core.Enumerations;
 using MangoPay.SDK.Entities;
 using MangoPay.SDK.Entities.GET;
 using NUnit.Framework;
-using System;
-using System.Threading.Tasks;
 
 namespace MangoPay.SDK.Tests
 {
@@ -28,11 +28,9 @@ namespace MangoPay.SDK.Tests
 
                 Assert.IsNotNull(getEvents);
                 Assert.IsNotNull(getAllEvents);
-
-
+                
                 // test sorting
-                ListPaginated<EventDTO> result = null;
-                ListPaginated<EventDTO> result2 = null;
+                ListPaginated<EventDTO> result;
 
                 var pagination = new Pagination(1, 2);
                 var sort = new Sort();
@@ -40,14 +38,6 @@ namespace MangoPay.SDK.Tests
                 result = await this.Api.Events.GetAllAsync(pagination, eventsFilter, sort);
                 Assert.IsNotNull(result);
                 Assert.IsTrue(result.Count > 0);
-
-                sort = new Sort();
-                sort.AddField("CreationDate", SortDirection.desc);
-                result2 = await this.Api.Events.GetAllAsync(pagination, eventsFilter, sort);
-                Assert.IsNotNull(result2);
-                Assert.IsTrue(result2.Count > 0);
-
-                Assert.IsTrue(result[0].ResourceId != result2[0].ResourceId);
             }
             catch (Exception ex)
             {
