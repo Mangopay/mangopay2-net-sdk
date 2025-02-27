19dc6bff4b8f043e9f25371508a51afc9f973f1a
Iulian Masar <iulian.masar@codegile.com>
2025-02-27 11:03:27 +0200

    updated the way rate limits are saved

--- MangoPay.SDK/Core/LastRequestInfo.cs
+++ MangoPay.SDK/Core/LastRequestInfo.cs
@@ -1,19 +1,27 @@
-﻿using RestSharp;
+﻿using System;
+using System.Collections.Generic;
+using RestSharp;
 
 namespace MangoPay.SDK.Core
 {
     public class LastRequestInfo
     {
-        public RestRequest Request { get; set; }    
+        public RestRequest Request { get; set; }
 
         public RestResponse Response { get; set; }
 
+        [Obsolete("This property only holds one of the values. Please use 'RateLimits' instead")]
         public string RateLimitingCallsAllowed { get; set; }
 
+        [Obsolete("This property only holds one of the values. Please use 'RateLimits' instead")]
         public string RateLimitingCallsMade { get; set; }
 
+        [Obsolete("This property only holds one of the values. Please use 'RateLimits' instead")]
         public string RateLimitingCallsRemaining { get; set; }
 
+        [Obsolete("This property only holds one of the values. Please use 'RateLimits' instead")]
         public string RateLimitingTimeTillReset { get; set; }
+
+        public List<RateLimit> RateLimits { get; set; }
     }
 }
