3a4366e8f3bff9650e1a182fc7fc0238e360a68b
Iulian Masar <iulian.masar@codegile.com>
2025-03-20 17:25:41 +0200

    small fixes

--- MangoPay.SDK/Entities/GET/UserLegalScaDTO.cs
+++ MangoPay.SDK/Entities/GET/UserLegalScaDTO.cs
@@ -11,36 +11,36 @@ namespace MangoPay.SDK.Entities.GET
     {
         /// <summary>Name of this user.</summary>
         public string Name { get; set; }
-        
+
         /// <summary>Type of legal user.</summary>
         [JsonConverter(typeof(StringEnumConverter))]
         public LegalPersonType LegalPersonType { get; set; }
-        
+
         /// <summary>Legal Representative</summary>
         public LegalRepresentative LegalRepresentative { get; set; }
-        
+
         /// <summary>Proof of registration.</summary>
         public string ProofOfRegistration { get; set; }
 
         /// <summary>Shareholder declaration.</summary>
         public string ShareholderDeclaration { get; set; }
-        
+
         /// <summary>Statute.</summary>
         public string Statute { get; set; }
-        
+
         /// <summary>Company Number</summary>
         public string CompanyNumber { get; set; }
-        
+
         /// <summary>
         /// Object containing the link needed for SCA redirection if triggered by the API call (otherwise returned null).
         /// </summary>
         public PendingUserActionDTO PendingUserAction { get; set; }
-        
+
         /// <summary>Headquarters address.</summary>
-		public Address HeadquartersAddress { get; set; }
-        
-		/// <summary>Legal representative address.</summary>
-		public Address LegalRepresentativeAddress { get; set; }
+        public Address HeadquartersAddress { get; set; }
+
+        /// <summary>Legal representative address.</summary>
+        public Address LegalRepresentativeAddress { get; set; }
 
         public bool? TermsAndConditionsAccepted { get; set; }
 
@@ -49,7 +49,7 @@ namespace MangoPay.SDK.Entities.GET
 
         [JsonConverter(typeof(StringEnumConverter))]
         public UserCategory UserCategory { get; set; }
-        
+
         [JsonConverter(typeof(StringEnumConverter))]
         public UserStatus UserStatus { get; set; }
     }
--- MangoPay.SDK/Entities/PUT/UserLegalScaPutDTO.cs
+++ MangoPay.SDK/Entities/PUT/UserLegalScaPutDTO.cs
@@ -8,29 +8,29 @@ namespace MangoPay.SDK.Entities.PUT
     /// <summary>User legal SCA PUT entity.</summary>
     public class UserLegalScaPutDTO : EntityPutBase
     {
-	    /// <summary>Name of this user.</summary>
-	    public string Name { get; set; }
-        
-	    /// <summary>Type of legal user.</summary>
-	    [JsonConverter(typeof(StringEnumConverter))]
-	    public LegalPersonType? LegalPersonType { get; set; }
-        
-	    /// <summary>Legal Representative</summary>
-	    public LegalRepresentative LegalRepresentative { get; set; }
-	    
-	    /// <summary>Company Number</summary>
-	    public string CompanyNumber { get; set; }
-	    
-	    /// <summary>Headquarters address.</summary>
-	    public Address HeadquartersAddress { get; set; }
-        
-	    /// <summary>Legal representative address.</summary>
-	    public Address LegalRepresentativeAddress { get; set; }
+        /// <summary>Name of this user.</summary>
+        public string Name { get; set; }
 
-	    public bool? TermsAndConditionsAccepted { get; set; }
-	    
-	    public string Tag { get; set; }
-	    
-	    public string Email { get; set; }
+        /// <summary>Type of legal user.</summary>
+        [JsonConverter(typeof(StringEnumConverter))]
+        public LegalPersonType? LegalPersonType { get; set; }
+
+        /// <summary>Legal Representative</summary>
+        public LegalRepresentative LegalRepresentative { get; set; }
+
+        /// <summary>Company Number</summary>
+        public string CompanyNumber { get; set; }
+
+        /// <summary>Headquarters address.</summary>
+        public Address HeadquartersAddress { get; set; }
+
+        /// <summary>Legal representative address.</summary>
+        public Address LegalRepresentativeAddress { get; set; }
+
+        public bool? TermsAndConditionsAccepted { get; set; }
+
+        public string Tag { get; set; }
+
+        public string Email { get; set; }
     }
 }
