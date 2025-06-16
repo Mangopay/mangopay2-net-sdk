959ca8da60e1b548909f7db45bc3c086523859c9
Iulian <40763147+iulian03@users.noreply.github.com>
2025-06-16 16:54:17 +0300

    virtual account updates (#268)
    - added new properties
    - changed InternationalAccount IBAN and BIC to Iban and Bic
    - removed unnecessary properties from VirtualAccountPostDTO
    
    Co-authored-by: Iulian Masar <iulian.masar@codegile.com>

--- MangoPay.SDK/Entities/LocalAccount.cs
+++ MangoPay.SDK/Entities/LocalAccount.cs
@@ -5,5 +5,19 @@ namespace MangoPay.SDK.Entities
         public string AccountNumber { get; set; }
 
         public string SortCode { get; set; }
+        
+        public string Iban { get; set; }
+
+        public string Bic { get; set; }
+        
+        public string AchNumber { get; set; }
+        
+        public string FedWireNumber { get; set; }
+        
+        public string AccountType { get; set; }
+        
+        public string BranchCode { get; set; }
+        
+        public string InstitutionNumber { get; set; }
     }
 }
