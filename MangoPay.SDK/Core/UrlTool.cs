f436412064e2b6d32854a5ccaf64df07bb95ffda
Iulian Masar <iulian.masar@codegile.com>
2025-07-04 15:22:00 +0300

    removed unused methods

--- MangoPay.SDK/Core/UrlTool.cs
+++ MangoPay.SDK/Core/UrlTool.cs
@@ -28,33 +28,6 @@ namespace MangoPay.SDK.Core
             return baseUrl.Authority;
         }
 
-   //      /// <summary>Gets REST url.</summary>
-   //      /// <param name="urlKey">Url key.</param>
-   //      /// <returns>Final REST url.</returns>
-   //      public string GetRestUrl(string urlKey)
-   //      {
-			// return GetRestUrl(urlKey, true, null, null, _root.Config.ApiVersion);
-   //      }
-   //
-   //      /// <summary>Gets REST url.</summary>
-   //      /// <param name="urlKey">Url key.</param>
-   //      /// <param name="addClientId">Denotes whether client identifier should be composed into final url.</param>
-   //      /// <returns>Final REST url.</returns>
-   //      public string GetRestUrl(string urlKey, Boolean addClientId)
-   //      {
-			// return GetRestUrl(urlKey, addClientId, null, null, _root.Config.ApiVersion);
-   //      }
-   //
-   //      /// <summary>Gets REST url.</summary>
-   //      /// <param name="urlKey">Url key.</param>
-   //      /// <param name="addClientId">Denotes whether client identifier should be composed into final url.</param>
-   //      /// <param name="pagination">Pagination object.</param>
-   //      /// <returns>Final REST url.</returns>
-   //      public string GetRestUrl(string urlKey, Boolean addClientId, Pagination pagination)
-   //      {
-			// return GetRestUrl(urlKey, addClientId, pagination, null, _root.Config.ApiVersion);
-   //      }
-
         /// <summary>Gets REST url.</summary>
         /// <param name="urlPath">Url path.</param>
         /// <param name="addClientId">Denotes whether client identifier should be composed into final url.</param>
