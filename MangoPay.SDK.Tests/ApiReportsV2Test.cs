aed2e607ca4867f2ea184cc4f5690f4e7fd359b1
Iulian <40763147+iulian03@users.noreply.github.com>
2025-08-07 15:35:17 +0300

    handle echo reports (#287)
    Co-authored-by: Iulian Masar <iulian.masar@codegile.com>

--- MangoPay.SDK.Tests/ApiReportsV2Test.cs
+++ MangoPay.SDK.Tests/ApiReportsV2Test.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
+using MangoPay.SDK.Core;
 using MangoPay.SDK.Entities;
+using MangoPay.SDK.Entities.POST;
 using NUnit.Framework;
 
 namespace MangoPay.SDK.Tests
@@ -65,6 +67,102 @@ namespace MangoPay.SDK.Tests
             {
                 Assert.Fail(ex.Message);
             }
+        }
+
+        [Test]
+        public async Task Test_Create_Intent_Report()
+        {
+            var reportPost = new ReportPostDTO
+            {
+                ReportType = "ECHO_INTENT",
+                AfterDate = DateTimeOffset.FromUnixTimeSeconds(1740787200).DateTime,
+                BeforeDate = DateTimeOffset.FromUnixTimeSeconds(1743544740).DateTime,
+                DownloadFormat = "CSV",
+                Filters = new ReportV2Filter
+                {
+                    Status = "CAPTURED",
+                    PaymentMethod = "PAYPAL",
+                    Type = "PAYIN"
+                }
+            };
+
+            var report = await this.Api.ReportsV2.CreateAsync(reportPost);
+            Assert.IsNotNull(report);
+            Assert.AreEqual("ECHO_INTENT", report.ReportType);
+            Assert.AreEqual("PENDING", report.Status);
+            Assert.AreEqual("CAPTURED", report.Filters.Status);
+        }
+        
+        [Test]
+        public async Task Test_Create_Intent_Action_Report()
+        {
+            var reportPost = new ReportPostDTO
+            {
+                ReportType = "ECHO_INTENT_ACTION",
+                AfterDate = DateTimeOffset.FromUnixTimeSeconds(1740787200).DateTime,
+                BeforeDate = DateTimeOffset.FromUnixTimeSeconds(1743544740).DateTime,
+                DownloadFormat = "CSV",
+                Filters = new ReportV2Filter
+                {
+                    Status = "CAPTURED",
+                    PaymentMethod = "PAYPAL",
+                    Type = "PAYIN"
+                }
+            };
+
+            var report = await this.Api.ReportsV2.CreateAsync(reportPost);
+            Assert.IsNotNull(report);
+            Assert.AreEqual("ECHO_INTENT_ACTION", report.ReportType);
+            Assert.AreEqual("PENDING", report.Status);
+            Assert.AreEqual("CAPTURED", report.Filters.Status);
+        }
+        
+        [Test]
+        public async Task Test_Create_Settlement_Report()
+        {
+            var reportPost = new ReportPostDTO
+            {
+                ReportType = "ECHO_SETTLEMENT",
+                AfterDate = DateTimeOffset.FromUnixTimeSeconds(1740787200).DateTime,
+                BeforeDate = DateTimeOffset.FromUnixTimeSeconds(1743544740).DateTime,
+                DownloadFormat = "CSV",
+                Filters = new ReportV2Filter
+                {
+                    Status = "RECONCILED",
+                    ExternalProviderName = "PAYPAL"
+                }
+            };
+
+            var report = await this.Api.ReportsV2.CreateAsync(reportPost);
+            Assert.IsNotNull(report);
+            Assert.AreEqual("ECHO_SETTLEMENT", report.ReportType);
+            Assert.AreEqual("PENDING", report.Status);
+            Assert.AreEqual("RECONCILED", report.Filters.Status);
+        }
+        
+        [Test]
+        public async Task Test_Create_Split_Report()
+        {
+            var reportPost = new ReportPostDTO
+            {
+                ReportType = "ECHO_SPLIT",
+                AfterDate = DateTimeOffset.FromUnixTimeSeconds(1740787200).DateTime,
+                BeforeDate = DateTimeOffset.FromUnixTimeSeconds(1743544740).DateTime,
+                DownloadFormat = "CSV",
+                Filters = new ReportV2Filter
+                {
+                    Status = "COMPLETED",
+                    IntentId = "int_0197f975-63f6-714e-8fc6-4451e128170f",
+                    Scheduled = false
+                }
+            };
+
+            var report = await this.Api.ReportsV2.CreateAsync(reportPost);
+            Assert.IsNotNull(report);
+            Assert.AreEqual("ECHO_SPLIT", report.ReportType);
+            Assert.AreEqual("PENDING", report.Status);
+            Assert.AreEqual("COMPLETED", report.Filters.Status);
+            Assert.AreEqual("int_0197f975-63f6-714e-8fc6-4451e128170f", report.Filters.IntentId);
         }
     }
 }
