44bd1c85a680c66f11a1f33090cbb89aef298421
Iulian Masar <iulian.masar@codegile.com>
2025-08-08 15:17:31 +0300

    handle client wallets conversions

---
+++ MangoPay.SDK/Entities/POST/ClientWalletsQuotedConversionPostDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace MangoPay.SDK.Entities.POST
+{
+    public class ClientWalletsQuotedConversionPostDTO : EntityPostBase
+    {
+        /// <summary>
+        /// The unique identifier of the active quote which guaranteed the rate for the conversion.
+        /// </summary>
+        public string QuoteId { get; set; }
+        
+        /// <summary>
+        /// The type of the client wallet to be debited: FEES or CREDIT
+        /// </summary>
+        public string DebitedWalletType { get; set; }
+
+        /// <summary>
+        /// The type of the client wallet to be credited: FEES or CREDIT
+        /// </summary>
+        public string CreditedWalletType { get; set; }
+    }
+}
